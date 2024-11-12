aa43ac509cbb8306779244ca2005b500a5849036
Jean-Michel Fayard <jmfayard@gmail.com>
2024-11-12 14:22:38 +0100

    Import from start.spring.io

---
+++ src/main/java/dev/jmfayard/gradle_playground/GradlePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package dev.jmfayard.gradle_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlePlaygroundApplication.class, args);
+	}
+
+}
