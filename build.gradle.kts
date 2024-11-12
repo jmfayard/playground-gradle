51cb83f9096d9abf013671ca5eb82152d0b909fd
Jean-Michel Fayard <jmfayard@gmail.com>
2024-11-12 15:21:45 +0100

    Create custom task

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,3 +28,53 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+
+tasks.register<Exec>("loc") {
+	commandLine("/opt/homebrew/bin/loc")
+}
+tasks.named("check").configure {
+	dependsOn("loc")
+}
+tasks.register<DefaultTask>("doctor") {
+	doLast {
+		val requiredEnvironmentVariables =  listOf("GITHUB_TOKEN")
+		// listOf("GITHUB_TOKEN_XXX", "DATABASE_URL", "JAVASCRIPT_IS_GREAT")
+		val requiredTools = mapOf(
+			"loc" to "Install with $ brew install loc",
+			// "brainfuck" to "See https://fr.wikipedia.org/wiki/Brainfuck",
+		)
+
+		gradleDoctorTask(requiredEnvironmentVariables, requiredTools)
+	}
+}
+
+
+
+
+
+
+
+
+fun gradleDoctorTask(requiredEnvironmentVariables: List<String>, requiredTools: Map<String, String>) {
+	println("Checking that the required environment variables are set")
+	val values = requiredEnvironmentVariables.associateWith {
+		System.getenv(it)
+	}
+	val missing = values.filter { it.value == null }.keys.toList()
+	println(values.entries.joinToString("\n"))
+	if (missing.isNotEmpty()) throw GradleException("Missing environment variables $missing")
+
+	println("Checking that the required tools are installed")
+	val paths = System.getenv("PATH").split(":").filterNotNull()
+	val map = requiredTools.keys.filter { cmd ->
+		val found = paths.firstOrNull { path ->
+			File(path, cmd).canExecute()
+		}
+		found == null
+	}
+	val missingTools = map.joinToString(separator = "\n") { tool ->
+		"MISSING TOOL $tool - ${requiredTools[tool]}"
+	}
+	if (map.isNotEmpty()) throw GradleException(missingTools)
+}
