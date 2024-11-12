809775fbe7a4e3576c09be5f3e24b622c731ad65
Jean-Michel Fayard <jmfayard@gmail.com>
2024-11-12 14:28:14 +0100

    Enable buildScan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,16 @@
+import com.gradle.develocity.agent.gradle.scan.BuildScanConfiguration
+
+plugins {
+    id("com.gradle.develocity") version "3.18.1"
+}
+
+// https://docs.gradle.com/develocity/gradle-plugin/current/
+develocity {
+    buildScan {
+        publishing.onlyIf { true }
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
+
 rootProject.name = "playground-gradle"
